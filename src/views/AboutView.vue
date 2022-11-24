b0a6d1192d64652bb8b2e3316dfde435c023e134
doniyor <erkinovdoniyor03@gmail.com>
2022-11-24 18:23:34 +0500

    vue typescript first look

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+     <p>{{name}} | {{age}}</p>
+    <button @click="changeName('Doniyor')">Change Name</button>
+    <button @click="changeAge(23)">Change Age</button>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue';
+export default defineComponent({
+  name: 'HomeView',
+  setup() {
+    const state = reactive({
+      name: 'Link',
+      age: 22 as string | number
+    })
+    return {
+      ...toRefs(state)
+    }
+    
+  },
+  methods: {
+     changeName(name: string) {
+      this.name = name
+    },
+    changeAge(age: number | string) {
+      this.age = age
+    } 
+  }
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <JobListComponentVue :jobs="jobs"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import { defineComponent, ref} from 'vue';
+import Job from '../types/Job'
+import JobListComponentVue from '@/components/JobList.component.vue';
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    JobListComponentVue
   },
+  setup() {
+    const jobs = ref<Job[]>([
+      {title: 'Frontend', location: 'Namangan', salary: 300, id: '1'},
+      {title: 'Logistics manager', location: 'Namangan', salary: 2000, id: '2'},
+      {title: 'Backend', location: 'Namangan', salary: 5000, id: '3'},
+      {title: 'Bank manager', location: 'Namangan', salary: 4000, id: '4'}
+    ])
+    return {
+      jobs
+    }
+  }
 });
 </script>
+<style>
+</style>
